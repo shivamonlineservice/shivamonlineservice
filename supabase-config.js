886d26f364c86edec3bd81964e2d6ee8cf6fb467
shivamonlineservice <shivamonlineservice27@gmail.com>
2026-09-10 20:20:08 +0530

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = "https://cvprugqgokeovlqgtfaok.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = sb_publishable_2vBgBPoX0uDbI8UM0LfjgQ_DCVDik9a
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_2vBgBPoX0uDbI8UM0LfjgQ_DCVDik9a";
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
